b5f47dea28799dc880b323a817579859e61d84dd
root <root@LAPTOP-EA6PCKJ1.localdomain>
2020-09-14 19:11:23 -0700

    prettier instalation and setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+function helloWorld( message:string){
+
+    console.log("Hello World");
+
+    let updatedMessage;
+
+    return 0;
+}
+
+
+helloWorld("goodbye world");
